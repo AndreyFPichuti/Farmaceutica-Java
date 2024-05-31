85ab388746357575ac586823341a6b7a862dc2c5
Andrey Ferreira Pichuti <72885888+AndreyFPichuti@users.noreply.github.com>
2024-05-31 14:54:55 -0300

    Add files via upload

---
+++ Farmaceutica/Farmaceutica/src/farmaceutica/FarmaceuticoDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package farmaceutica;
+
+/**
+ *
+ * @author Andrey
+ */
+public class FarmaceuticoDAO {
+    //public void registrarRemedio() {}
+    //public void pesquisarRemedio() {}
+    //public void gerarRelatorio() {}
+}
