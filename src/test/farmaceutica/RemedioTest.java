63d7030e95d8e88a8f8d3e492eb781217f6cb388
João Gabriel <joaogabrielb840@gmail.com>
2025-11-15 16:30:54 -0300

    Adicionando teste de Setters em RemedioTest com o objetivo de garantir que o Remédio pode ser modificado depois de criado

--- src/test/farmaceutica/RemedioTest.java
+++ src/test/farmaceutica/RemedioTest.java
@@ -32,5 +32,36 @@ public class RemedioTest {
         assertNotNull(r.getPacientes());
     }
 
+    @Test
+    void testSetters() {
+        Remedio r = new Remedio();
+        Farmaceutico f = new Farmaceutico(2, "gabriel@example.com", "senha");
+
+        r.setIdFarmaceutico(2);
+        r.setIdRemedio(20);
+        r.setNome("Ibuprofeno");
+        r.setEstoque(100);
+        r.setViaMedicacao("Oral");
+        r.setIndicacoes("Inflamação");
+        r.setContraindicacoes("Úlcera");
+        r.setPrincipalAtivo("Ibuprofeno");
+        r.setEfeitosColaterais("Tontura");
+        r.setFormaFarmaceutica("Cápsula");
+        r.setValidade("2026-01-01");
+        r.setFarmaceutico(f);
+
+        assertEquals(2, r.getIdFarmaceutico());
+        assertEquals(20, r.getIdRemedio());
+        assertEquals("Ibuprofeno", r.getNome());
+        assertEquals(100, r.getEstoque());
+        assertEquals("Oral", r.getViaMedicacao());
+        assertEquals("Inflamação", r.getIndicacoes());
+        assertEquals("Úlcera", r.getContraindicacoes());
+        assertEquals("Ibuprofeno", r.getPrincipalAtivo());
+        assertEquals("Tontura", r.getEfeitosColaterais());
+        assertEquals("Cápsula", r.getFormaFarmaceutica());
+        assertEquals("2026-01-01", r.getValidade());
+        assertEquals(f, r.getFarmaceutico());
+    }
 
 }
