b05d2df2c64ac42f28a165b225cdbd386d0c904c
Andrey Ferreira Pichuti <tpichuti@gmail.com>
2025-11-19 15:25:24 -0300

    Teste construtor do paciente feito

---
+++ src/test/farmaceutica/PacienteTest.java
@@ -0,0 +1,20 @@
+package farmaceutica.test;
+
+import farmaceutica.Paciente;
+import farmaceutica.Medico;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PacienteTest {
+
+    @Test
+    void testConstrutorCompletoEGetters() {
+        Medico m = new Medico();
+        Paciente p = new Paciente("Maria", "11122233344", 1, m);
+        
+        assertEquals("Maria", p.getNome());
+        assertEquals("11122233344", p.getCpf());
+        assertEquals(1, p.getIdMedico());
+        assertEquals(m, p.getMedico());
+    }
+}
