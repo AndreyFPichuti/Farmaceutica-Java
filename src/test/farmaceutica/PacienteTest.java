275aa7e7718654475148d76672dd2352e90c9d38
Andrey Ferreira Pichuti <tpichuti@gmail.com>
2025-11-19 15:26:22 -0300

    Teste setters do paciente feito

--- src/test/farmaceutica/PacienteTest.java
+++ src/test/farmaceutica/PacienteTest.java
@@ -17,4 +17,21 @@ public class PacienteTest {
         assertEquals(1, p.getIdMedico());
         assertEquals(m, p.getMedico());
     }
+
+    @Test
+    void testSetters() {
+        Paciente p = new Paciente();
+        Medico m = new Medico();
+        
+        p.setNome("Gabriel");
+        p.setCpf("99988877766");
+        p.setIdMedico(1);
+        p.setMedico(m);
+
+        
+        assertEquals("Gabriel", p.getNome());
+        assertEquals("99988877766", p.getCpf());
+        assertEquals(1, p.getIdMedico());
+        assertEquals(m, p.getMedico());
+    }
 }
