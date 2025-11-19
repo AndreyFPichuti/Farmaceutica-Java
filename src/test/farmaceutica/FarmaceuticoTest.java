aff80d7f266f6713e61a02deb548aa0d37923019
Moshe <achkiymoshe@gmail.com>
2025-11-19 15:10:25 -0300

    test setters farmaceutico feito

--- src/test/farmaceutica/FarmaceuticoTest.java
+++ src/test/farmaceutica/FarmaceuticoTest.java
@@ -14,4 +14,17 @@ public class FarmaceuticoTest {
         assertEquals("joao@example.com", f.getEmail());
         assertEquals("1234", f.getSenha());
     }
+    
+    @Test
+    void testSetters() {
+        Farmaceutico f = new Farmaceutico();
+
+        f.setIdFarmaceutico(5);
+        f.setEmail("marcos@example.com");
+        f.setSenha("abcd");
+
+        assertEquals(5, f.getIdFarmaceutico());
+        assertEquals("marcos@example.com", f.getEmail());
+        assertEquals("abcd", f.getSenha());
+    }
 }
